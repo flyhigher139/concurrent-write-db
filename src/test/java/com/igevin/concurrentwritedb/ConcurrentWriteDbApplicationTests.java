ecdb800655784847d4ec8abbc60421083d755e7e
gevin <flyhigher139@gmail.com>
2022-03-28 21:15:53 +0800

    init

---
+++ src/test/java/com/igevin/concurrentwritedb/ConcurrentWriteDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igevin.concurrentwritedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentWriteDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
